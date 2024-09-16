e08d531a994f0fb233df483ac1a6e63db81141b5
P3nn4 <fpennadesouza@gmail.com>
2024-09-16 20:22:09 -0300

    initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+
+int main(void){
+    setlocale(LC_ALL, "Portuguese");
+    float codigodoproduto[21];
+    float peso;
+
+printf("Digite o código do produto: ");
+scanf("%f", &codigodoproduto);
+printf("Digite o peso do produto: ");
+scanf("%f", &peso);
+
+
+}
