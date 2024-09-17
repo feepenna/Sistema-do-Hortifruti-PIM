a22cfd9dc6f9e87cb604373b06c4340a0d87cf77
P3nn4 <fpennadesouza@gmail.com>
2024-09-17 10:44:55 -0300

    variaveis de vetor

--- main.c
+++ main.c
@@ -4,13 +4,17 @@
 
 int main(void){
     setlocale(LC_ALL, "Portuguese");
-    char codigodoproduto [20];
+    float codigodoproduto [20];
     float peso;
 
+//int legumes[10];
+//int verduras[5];
+//int vegetal[5];
 printf("Digite o código do produto: ");
 scanf("%f", &codigodoproduto);
 printf("Digite o peso do produto: ");
 scanf("%f", &peso);
+return 0;
 
 
 }
