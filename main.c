44386a9d3ed36fe3b34c9958ed7cb8c40f259f78
P3nn4 <fpennadesouza@gmail.com>
2024-09-25 17:45:28 -0300

    Initial commit Nesse commit fiz tudo denovo para realmente pegar como funcionam as coisas e estruturar o programar de como eu acho q ele deve funcionar(pelo menos na base)

--- main.c
+++ main.c
@@ -2,19 +2,47 @@
 #include <locale.h>
 #include <stdlib.h>
 
-int main(void){
+int main( ){
     setlocale(LC_ALL, "Portuguese");
-    float codigodoproduto [20];
-    float peso;
+    float umquilovt = 5.0, umquilovr = 4.0, umquilole = 3.0, peso;
+    //cada produto custa 5 reais o quilo, com a variável peso elas vão se multiplicar ent se alguém compra dois quilos de manjerição vão ser 10 reais
+    char calculo;
+    float preco;
 
-//int legumes[10];
-//int verduras[5];
-//int vegetal[5];
-printf("Digite o código do produto: ");
-scanf("%f", &codigodoproduto);
-printf("Digite o peso do produto: ");
-scanf("%f", &peso);
-return 0;
+    do{
+        printf("Qual produto deseja escanear?\n");
+        printf("v. vegetais \n");
+        printf("r. verduras \n");
+        printf("l. legumes \n");
+        printf("e. saída \n");
+        scanf(" %c", &calculo);
 
+        switch (calculo){
+        case 'v':
+            printf("Digite o peso do produto: ");
+            scanf("%f", &peso);
+            preco = umquilovt*peso;
+            break;
+        case 'r':
+            printf("Digite o peso do produto: ");
+            scanf("%f", &peso);
+            preco = umquilovr*peso;
+            break;
+        case 'l':
+            printf("Digite o peso do produto: ");
+            scanf("%f", &peso);
+            preco = umquilole*peso;
+            break;
+        case 'e':
+            printf("Sair \n");
+            break;
+        default:
+            printf("Operação inválida \n");
+            break;
+        }
+    }while (calculo != 'e');
+        printf("Fim da execucaõ");
 
-}
+        return 0;
+    }
+    
