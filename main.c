185d3ff1a1cbe0cdaf1e4e6d071e35e7d8e2851c
P3nn4 <fpennadesouza@gmail.com>
2024-09-26 13:04:00 -0300

    add: adicionei as funções para exibir o valor correspondente a multiplicação dos valores pré estabelecidos e do peso digitado pelo usuário

--- main.c
+++ main.c
@@ -1,48 +1,47 @@
 #include <stdio.h>
 #include <locale.h>
-#include <stdlib.h>
 
-int main( ){
+int main() {
     setlocale(LC_ALL, "Portuguese");
-    float umquilovt = 5.0, umquilovr = 4.0, umquilole = 3.0, peso;
-    //cada produto custa 5 reais o quilo, com a variável peso elas vão se multiplicar ent se alguém compra dois quilos de manjerição vão ser 10 reais
+    double umquilovt = 5.0, umquilovr = 4.0, umquilole = 3.0, peso;
     char calculo;
-    float preco;
+    double preco;
 
     do{
         printf("Qual produto deseja escanear?\n");
         printf("v. vegetais \n");
         printf("r. verduras \n");
         printf("l. legumes \n");
-        printf("e. saída \n");
+        printf("e. saida \n");
         scanf(" %c", &calculo);
 
         switch (calculo){
         case 'v':
             printf("Digite o peso do produto: ");
-            scanf("%f", &peso);
-            preco = umquilovt*peso;
+            scanf("%lf", &peso);
+            preco = umquilovt * peso;
             break;
         case 'r':
             printf("Digite o peso do produto: ");
-            scanf("%f", &peso);
-            preco = umquilovr*peso;
+            scanf("%lf", &peso);
+            preco = umquilovr * peso;
             break;
         case 'l':
             printf("Digite o peso do produto: ");
-            scanf("%f", &peso);
-            preco = umquilole*peso;
+            scanf("%lf", &peso);
+            preco = umquilole * peso;
             break;
         case 'e':
-            printf("Sair \n");
+            //printf("Sair \n");
             break;
         default:
             printf("Operação inválida \n");
             break;
         }
-    }while (calculo != 'e');
-        printf("Fim da execucaõ");
 
-        return 0;
+        printf("O preço total é: %.2f\n", preco);
+
+    }while (calculo != 'e');
+    printf("Fim da execução\n");
     }
-    
+
